83957dd8d07ae9f68886730bdc97e236e632d340
Vivek-Kumar-1907 <vivekkkrumar2007@gmail.com>
2025-08-13 12:05:30 +0530

    Added cprog assignment codes

---
+++ ee25btech11062/cprog/Problem1/codes/main.c
@@ -0,0 +1,10 @@
+//NCERT problem 2.2.44 - EE25BTECH11062
+//Dividing a number by a fraction
+#include <stdio.h>
+
+int main(void){
+	float a = 4.0;
+	float b = 8.0/3.0;
+	printf("The answer is : %.2f\n", a/b);
+	return 0;
+}
