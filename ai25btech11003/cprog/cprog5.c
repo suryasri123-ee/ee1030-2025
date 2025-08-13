0253c092efab0ae2ec9256f6c239132767b412ef
BhaveshG29072007 <bhaveshpgaikwad@gmail.com>
2025-08-13 18:55:17 +0530

    cprog assignment

---
+++ ai25btech11003/cprog/cprog5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+float c= 31.4;
+float r = c/(3.14*2);
+
+float a= 3.14*r*r;
+
+printf("\n\n If the circumference of a circle is 3.14cm then its Radius is %.1f cm and its Area is %.2f cm2. \n\n" , r ,a);
+
+
+return 0;
+}
