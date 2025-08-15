aca7880bdf49bf34d4901e8f6931893991d9e2d7
AryansinghSonaye <aryansonaye@gmail.com>
2025-08-16 01:08:58 +0530

    Revised assignment

--- ai25btech11032/C_prog/test4.c
+++ ai25btech11032/C_prog/test4.c
@@ -1,18 +1,27 @@
 #include<stdio.h>
 
+
+void inv(float matrix[2][2] , float inverse[2][2]){
+
+	float det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+
+	float invDet = 1.0 / det;
+
+    inverse[0][0] =  matrix[1][1] * invDet;
+    inverse[0][1] = -matrix[0][1] * invDet;
+    inverse[1][0] = -matrix[1][0] * invDet;
+    inverse[1][1] =  matrix[0][0] * invDet;
+}
+
+
+
 int main(){
 	float A[2][2] = {{1,1},{-1,1}};
 	float B[2]={180,44};
 	float invA[2][2];
-	float det;
 	float X[2];
 
-	det=A[0][0]*A[1][1] - A[0][1]*A[1][0];
-
-	invA[0][0]= A[1][1] / det;
-	invA[0][1]= -A[0][1] / det;
-	invA[1][0]= -A[1][0] / det;
-	invA[1][1]= A[0][0] / det;
+	inv(A,invA);
 
 	X[0]=invA[0][0]*B[0] + invA[0][1]*B[1];
 	X[1]=invA[1][0]*B[0] + invA[1][1]*B[1];
@@ -22,3 +31,4 @@ int main(){
 
 	return 0;
 	}
+
