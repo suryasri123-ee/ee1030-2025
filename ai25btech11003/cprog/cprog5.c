f0f4ef5c700e0e4423079edc8c4b69ac2b2ff9cd
BhaveshG29072007 <bhaveshpgaikwad@gmail.com>
2025-08-15 23:25:41 +0530

    cprog5.c updated

--- ai25btech11003/cprog/cprog5.c
+++ ai25btech11003/cprog/cprog5.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+#include<math.h>
 int main() {
 
 float c= 31.4;
-float r = c/(3.14*2);
+float r = c/(M_PI*2);
 
-float a= 3.14*r*r;
+float a= M_PI*r*r;
 
 printf("\n\n If the circumference of a circle is 3.14cm then its Radius is %.1f cm and its Area is %.2f cm2. \n\n" , r ,a);
 
