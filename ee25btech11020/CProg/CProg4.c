08cf3a25889f2c4ff157857a47d62e29dcb09623
gajaredarsh <darshgajare@gmail.com>
2025-08-19 18:51:11 +0530

    ee25btech11020

--- ee25btech11020/CProg/CProg4.c
+++ ee25btech11020/CProg/CProg4.c
@@ -3,52 +3,73 @@
 //Drawing bar graph and finding max and min
 #include <stdio.h>
 #include <stdlib.h>
+#include "datafun.h"
+#define MAX_HEIGHT 10
 // declaring function
-void compare(char *colours[], int students[], int n);
 void printBarGraph(char *colours[], int students[], int n);
+void bar(int val, int thresh);
+void compare(char *colours[], int students[], int n);
 // main function
 int main()
 {
 	char *colours[] = {"Red", "Green", "Blue", "Yellow", "Orange"};
 	int students[] = {43, 19, 55, 49, 34};
 	int n=5;
-	printBarGraph(colours, students,n);
-	compare(colours,students,n);
+	printBarGraph(colours,students,n);
+	compare(colours, students, n);
 	return 0;
 }
-
-// Finding most and least preferred colour
 void compare(char *colours[], int students[], int n)
 {
-	int min, max;
-	for(int i=1;i<n;i++)
+	int max,min=0;
+	for(int i=0; i<n; i++)
 	{
-		if (students[i] > students[max])
+		if(students[i]>students[max])
 			max=i;
-		if (students[i] < students[min])
+		if(students[i]<students[min])
 			min=i;
 	}
-	printf("Most preferred colour: %s \n", colours[max]);
-	printf("Least preferred colour: %s \n", colours[min]);
+	printf("Most preferred colour is %s\n",colours[max]);
+	printf("Least preferred colour is %s\n",colours[min]);
+	printf("%d is the total number of colours\n",n);
+	for(int i=0; i<n; i++)
+	printf("%s ",colours[i]);
+	printf("\n");
 }
-//Printing Bar graph
-void printBarGraph(char *colours[], int students[],int n)
+void printBarGraph(char *colours[], int students[], int n)
 {
-	int i, j , maxheight;
-	for(i=0;i<n;i++)
-		if (students[i]>maxheight)
-		{	maxheight = students[i];}
-	for (i=maxheight;i>=1;i--)
+	//Find maximum value
+	int l=max(students,n);
+	//bar graph scaling factor
+	float scale=(l>MAX_HEIGHT)?(float)MAX_HEIGHT/l :1.0;
+	//Draw the bars vertically
+	for(int row=MAX_HEIGHT; row>0 ; row--)
 	{
-		for(j=0;j<n;j++){
-			if(students[j]>=i)
-				printf("\t*\t");
-			else
-				printf("\t \t");
+		for (int i=0;i<n;i++)
+		{
+			printf(" ");
+			bar((int)(students[i]*scale),row);
 		}
 		printf("\n");
 	}
-	for (i=0;i<n;i++)
-		printf("%-5s \t\t",colours[i]);
+	//draw X-axis
+	for(int i=0;i<n;i++)
+	{
+		printf("---");
+	}
 	printf("\n");
+	//show your labels
+	for(int i=0;i<n;i++)
+	{
+		printf("%s ",colours[i]);
+	}
+	printf("\n");
+}
+//drawing bars
+void bar(int val, int thresh)
+{
+	if(val>=thresh)
+		printf("| ");
+	else
+		printf("  ");
 }
