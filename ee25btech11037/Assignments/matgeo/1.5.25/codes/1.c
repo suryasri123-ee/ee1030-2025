1fcf68c3f90ffdca1a588b25d595f2b9e4b9ec49
Divyansh <toothless9328@gmail.com>
2025-08-23 16:38:21 +0530

    1

--- ee25btech11037/Assignments/matgeo/1.5.25/codes/1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-double calculate_lambda(double xp, double xq, double xr) {
-    if (xr - xq == 0.0) {
-        printf("Error: Division by zero. xr cannot be equal to xq.\n");
-        return -1.0; // Return an error value
-    }
-    return (xp - xr) / (xr - xq);
-}
