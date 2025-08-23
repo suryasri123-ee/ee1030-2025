fd01496fbb4be79e84cc17fdd1cf74bf01b62427
HarshaBJ2007 <bjharsha2007@gmail.com>
2025-08-23 20:20:29 +0530

    ee25btech11026

--- ee25btech11026-2/matgeo/1.9.15/Codes/magnitude_sum.c
+++ ee25btech11026-2/matgeo/1.9.15/Codes/magnitude_sum.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 
-double find_magnitude(int result[3])
+double find_magnitude(int *result)
 {
 	double mag;
 	mag=sqrt(pow(result[0],2)+pow(result[1],2)+pow(result[2],2));
@@ -9,7 +9,7 @@ double find_magnitude(int result[3])
 
 }
 
-void sum_of_vectors(int a[3], int b[3], int c[3], int result[3])
+void sum_of_vectors(int *a, int *b, int *c, int *result)
 {
     for(int i = 0; i < 3; i++)
     {
