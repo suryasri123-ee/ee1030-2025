0024b8e5ab01bd69ffa5eeaea830478b002a2ab8
aditya.appana.aa@gmail.com <adityaappana@aims.iith.ac.in>
2025-08-28 22:52:17 +0530

    Assignment3

--- ee25btech11004/matgeo/2.3.7/Codes/PurePythonCode3.py
+++ ee25btech11004/matgeo/2.3.7/Codes/PurePythonCode3.py
@@ -22,41 +22,23 @@ angle = math.asin(cos)
 
 print(angle*180/3.1415)
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-# 1. Set up the figure and 3D axes
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# 2. Define the plane equation: 2x + 3y + 4z = 12
-#    We will solve for z: z = (12 - 2x - 3y) / 4
-
-# 3. Create a grid of x and y values
-#    np.linspace creates an array of evenly spaced numbers
 x = np.linspace(-10, 10, 20)
 y = np.linspace(-10, 10, 20)
 
-#    np.meshgrid creates coordinate matrices from coordinate vectors
 X, Y = np.meshgrid(x, y)
 
-# 4. Calculate the corresponding Z values
 Z = (X-2*Y- 1)/2
 z1 = (6*Y - 3*X)/2
 
-# 5. Plot the surface
-#    `plot_surface` creates the 3D plane
-#    `alpha` sets the transparency of the plane
-#    `cmap` sets the color map
 ax.plot_surface(X, Y, Z, alpha=1, cmap='Blues')
 ax.plot_surface(X, Y, z1, alpha=1, cmap='Oranges')
 
-# 6. Add labels and a title for clarity
 ax.set_xlabel('X axis')
 ax.set_ylabel('Y axis')
 ax.set_zlabel('Z axis')
 ax.set_title('Plot of the planes')
 
-# 7. Display the plot
 plt.show()
