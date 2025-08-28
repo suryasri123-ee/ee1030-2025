481599c88a7e2cc70cfce2c32c950c2d2f94b47c
vishwambhar-ee25btech11025 <vishwambhar.450@gmail.com>
2025-08-28 09:33:08 +0530

    matgeo 1.5.13

--- EE25BTECH11025/ASSIGNMENTS/matgeo/1.9.14/codes/problem.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void get_points(double *points) {
-    points[0] = 5;   points[1] = -8;   // R
-    points[2] = -4;  points[3] = -4;   // Q
-    points[4] = 2;   points[5] = 2;    // P
-}
-
